25952044358795f27e05da63a9122292ba2968fb
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2026-07-22 18:37:50 +0800

    Fix Rubocop formatting

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get "courses/index"
   get "courses/show"
   root "home#index"
-  resources :courses, only: [:index, :show]
-  resources :lessons, only: [:show]
+  resources :courses, only: [ :index, :show ]
+  resources :lessons, only: [ :show ]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
