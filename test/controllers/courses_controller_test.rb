0a4a85d22dc25d8e4d75c21b7c25ffa1241201c0
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2026-07-22 18:41:58 +0800

    Add system test directory

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class CoursesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get courses_index_url
+    get course_url(1)
     assert_response :success
   end
 
